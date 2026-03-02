d83f6be055e6f16ae8dbbd789409cb77ed48fa31
gold2201 <dimaolha6@gmail.com>
2026-03-02 15:11:03 +0300

    Очистка кэша

--- network_project/urls.py
+++
@@ -1,8 +0,0 @@
-# network_project/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('network_api.urls')),  # Включаем ВСЕ пути из приложения
-]
